13c1dcf9e36c26711b6307c5bc218a3cdbd07e15
parsad <par.sadeghi@yahoo.com>
2017-05-18 18:48:17 +1000

    Add files via upload

--- app.js
+++ app.js
@@ -9,14 +9,17 @@ app.controller('myCtrl', function($scope) {
 	$scope.value = [0,1,2,3,4,5,6,7,8,9,".","+","-","*","/"];
 	$scope.result="";
 	
+	// a function to show all values in the screen
 	$scope.display = function(value){
 		$scope.result += $scope.value[value];
 	};
 
+	//a function to clear screen
 	$scope.clear = function(){
 		$scope.result = "";
 	};
 
+	//a function to calculate operations
 	$scope.equal = function(){
 		$scope.result = $scope.$eval($scope.result);
 	};
